0b50bd9689b0e30d147583ffdd50235b006e76bb
Gonzalo <mgdammy14@gmail.com>
2023-06-30 14:06:10 -0500

    nuevos ingresos pt1

--- src/Data.js
+++ src/Data.js
@@ -1,164 +1,392 @@
 const dataCompleta = [
   {
-    id: 1,
-    nombresCompletos: "Anyu Francesca Alejandria Solari",
-    rifas: 1,
+  id: 1,
+  nombresCompletos: "Daniel Alberto Cardenas Perez",
+  rifas: 1,
   },
   {
-    id: 2,
-    nombresCompletos: "Daniel Alberto Cardenas Pérez",
-    rifas: 1,
+  id: 2,
+  nombresCompletos: "Candy Loriet Zegarra Castillo",
+  rifas: 1,
   },
   {
-    id: 3,
-    nombresCompletos: "Candy Loriet Zegarra Castillo",
-    rifas: 1,
+  id: 3,
+  nombresCompletos: "George Antony Garcia Congona",
+  rifas: 1,
   },
   {
-    id: 4,
-    nombresCompletos: "George Antony García Congona",
-    rifas: 1,
+  id: 4,
+  nombresCompletos: "Yovana Milagros Montesinos Ortega",
+  rifas: 1,
   },
   {
-    id: 5,
-    nombresCompletos: "Adrian Nicolás Meléndez",
-    rifas: 1,
+  id: 5,
+  nombresCompletos: "Adrian Nicolas Melendez",
+  rifas: 1,
   },
   {
-    id: 6,
-    nombresCompletos: "Nilda Beatríz Herrera Peña",
-    rifas: 1,
+  id: 6,
+  nombresCompletos: "Martina Chinchay Ticliahuanca",
+  rifas: 1,
   },
   {
-    id: 7,
-    nombresCompletos: "Martina Chinchay Ticliahuanca",
-    rifas: 1,
+  id: 7,
+  nombresCompletos: "Itamar Solano Huertas",
+  rifas: 1,
   },
   {
-    id: 8,
-    nombresCompletos: "Itamar Solano Huertas",
-    rifas: 1,
+  id: 8,
+  nombresCompletos: "Naomi Vasquez Aquino",
+  rifas: 1,
   },
   {
-    id: 9,
-    nombresCompletos: "Naomí Vasquez Aquino",
-    rifas: 1,
+  id: 9,
+  nombresCompletos: "Juan Francisco Taipe Rojas",
+  rifas: 1,
   },
   {
-    id: 10,
-    nombresCompletos: "Juan Francisco Taipe Rojas",
-    rifas: 1,
+  id: 10,
+  nombresCompletos: "Ercida Rosa Roncal Segura",
+  rifas: 1,
   },
   {
-    id: 11,
-    nombresCompletos: "Ercida Rosa Roncal Segura",
-    rifas: 2,
+  id: 11,
+  nombresCompletos: "Ercida Rosa Roncal Segura",
+  rifas: 1,
   },
   {
     id: 12,
     nombresCompletos: "Edwin Roberto Guerrero Merino",
-    rifas: 5,
-  },
-  {
+    rifas: 1
+    },
+    {
     id: 13,
-    nombresCompletos: "Karla Calderón Retamozo",
-    rifas: 1,
-  },
-  {
+    nombresCompletos: "Edwin Roberto Guerrero Merino",
+    rifas: 1
+    },
+    {
     id: 14,
-    nombresCompletos: "Jovita Ysidora Gutiérrez Barreto",
-    rifas: 1,
-  },
-  {
+    nombresCompletos: "Edwin Roberto Guerrero Merino",
+    rifas: 1
+    },
+    {
     id: 15,
-    nombresCompletos: "Bertha Maritza Rojas Moztacero",
-    rifas: 1,
-  },
-  {
+    nombresCompletos: "Edwin Roberto Guerrero Merino",
+    rifas: 1
+    },
+    {
     id: 16,
-    nombresCompletos: "Edmundo Calderón Pérez (César)",
-    rifas: 5,
-  },
-  {
+    nombresCompletos: "Edwin Roberto Guerrero Merino",
+    rifas: 1
+    },
+    {
     id: 17,
-    nombresCompletos: "Héctor Armando Enciso Vera",
-    rifas: 1,
-  },
-  {
+    nombresCompletos: "Karla Calderon Retamozo",
+    rifas: 1
+    },
+    {
     id: 18,
-    nombresCompletos: "Raffo Gómez Falcón",
-    rifas: 1,
-  },
-  {
+    nombresCompletos: "Jovita Ysidora Gutierrez Barreto",
+    rifas: 1
+    },
+    {
     id: 19,
-    nombresCompletos: "Nancy Garay Torres",
-    rifas: 1,
-  },
-  {
+    nombresCompletos: "Bertha Maritza Rojas Mostacero",
+    rifas: 1
+    },
+    {
     id: 20,
-    nombresCompletos: "Jennifer Yahuarcani Ahuanari",
-    rifas: 1,
-  },
-  {
+    nombresCompletos: "(Cesar) Edmundo Calderon Perez",
+    rifas: 1
+    },
+    {
     id: 21,
-    nombresCompletos: "Mariel Barreros M.",
-    rifas: 1,
-  },
-  {
+    nombresCompletos: "(Cesar) Edmundo Calderon Perez",
+    rifas: 1
+    },
+    {
     id: 22,
-    nombresCompletos: "Danitza Alejandra",
-    rifas: 1,
-  },
-  {
+    nombresCompletos: "(Cesar) Edmundo Calderon Perez",
+    rifas: 1
+    },
+    {
     id: 23,
-    nombresCompletos: "Abel Garay Torres",
-    rifas: 1,
-  },
-  {
+    nombresCompletos: "(Cesar) Edmundo Calderon Perez",
+    rifas: 1
+    },
+    {
     id: 24,
-    nombresCompletos: "Mayra Azucena Matos",
-    rifas: 1,
-  },
-  {
+    nombresCompletos: "(Cesar) Edmundo Calderon Perez",
+    rifas: 1
+    },
+    {
     id: 25,
-    nombresCompletos: "Adderly Limber Alejos Ivin",
-    rifas: 2,
-  },
-  {
+    nombresCompletos: "Hector Armando Enciso Vera",
+    rifas: 1
+    },
+    {
     id: 26,
-    nombresCompletos: "Rosa Elena Márquez Fajardo",
-    rifas: 1,
-  },
-  {
+    nombresCompletos: "Raffo Gomez Falcon",
+    rifas: 1
+    },
+    {
     id: 27,
-    nombresCompletos: "Anthony Carlos Vera",
-    rifas: 3,
-  },
-  {
+    nombresCompletos: "Mariel Barreros M*",
+    rifas: 1
+    },
+    {
     id: 28,
-    nombresCompletos: "Maribel Mejia Campos",
-    rifas: 2,
-  },
-  {
+    nombresCompletos: "Danitza Alexandra Bedoya Ferrer*",
+    rifas: 1
+    },
+    {
     id: 29,
-    nombresCompletos: "Martha Vásquez Miñano",
-    rifas: 1,
-  },
-  {
+    nombresCompletos: "Abel Garay Torres ",
+    rifas: 1
+    },
+    {
     id: 30,
-    nombresCompletos: "Shadia Abushaibe Thride",
-    rifas: 2,
-  },
-  {
+    nombresCompletos: "Mayra Azucena Mattos",
+    rifas: 1
+    },
+    {
     id: 31,
-    nombresCompletos: "Irma Ninaquispe Terrones",
-    rifas: 1,
-  },
-  {
+    nombresCompletos: "Adderly Limber Alejos Ñivin*",
+    rifas: 1
+    },
+    {
     id: 32,
-    nombresCompletos: "Ruben Darío Romero Meza",
-    rifas: 4,
-  },
-];
-
-export default dataCompleta;
+    nombresCompletos: "Adderly Limber Alejos Ñivin*",
+    rifas: 1
+    },
+    {
+    id: 33,
+    nombresCompletos: "Rosa Elena Marquez Fajardo*",
+    rifas: 1
+    },
+    {
+    id: 34,
+    nombresCompletos: "Anthony Carlos Vera*",
+    rifas: 1
+    },
+    {
+    id: 35,
+    nombresCompletos: "Anthony Carlos Vera*",
+    rifas: 1
+    },
+    {
+    id: 36,
+    nombresCompletos: "Anthony Carlos Vera*",
+    rifas: 1
+    },
+    {
+    id: 37,
+    nombresCompletos: "Marivel Mejia Campos*",
+    rifas: 1
+    },
+    {
+    id: 38,
+    nombresCompletos: "Marivel Mejia Campos*",
+    rifas: 1
+    },
+    {
+    id: 39,
+    nombresCompletos: "Martha Vasquez Miñano*",
+    rifas: 1
+    },
+    {
+    id: 40,
+    nombresCompletos: "Shadia Abushaibe Thride*",
+    rifas: 1
+    },
+    {
+    id: 41,
+    nombresCompletos: "Shadia Abushaibe Thride*",
+    rifas: 1
+    },
+    {
+    id: 42,
+    nombresCompletos: "Irma Ninaquispe Terrones*",
+    rifas: 1
+    },
+    {
+    id: 43,
+    nombresCompletos: "Ruben Dario Romero Meza*",
+    rifas: 1
+    },
+    {
+    id: 44,
+    nombresCompletos: "Ruben Dario Romero Meza*",
+    rifas: 1
+    },
+    {
+      id: 45,
+      nombresCompletos: "Ruben Dario Romero Meza*",
+      rifas: 1
+      },
+      {
+      id: 46,
+      nombresCompletos: "Ruben Dario Romero Meza*",
+      rifas: 1
+      },
+      {
+      id: 47,
+      nombresCompletos: "Henry Salas*",
+      rifas: 1
+      },
+      {
+      id: 48,
+      nombresCompletos: "Henry Salas*",
+      rifas: 1
+      },
+      {
+      id: 49,
+      nombresCompletos: "Jazzmin Cabrera Acosta*",
+      rifas: 1
+      },
+      {
+      id: 50,
+      nombresCompletos: "Jazzmin Cabrera Acosta*",
+      rifas: 1
+      },
+      {
+      id: 51,
+      nombresCompletos: "Jazzmin Cabrera Acosta*",
+      rifas: 1
+      },
+      {
+      id: 52,
+      nombresCompletos: "Sonia Garcia*",
+      rifas: 1
+      },
+      {
+      id: 53,
+      nombresCompletos: "Genesis Dubraska Serrano*",
+      rifas: 1
+      },
+      {
+      id: 54,
+      nombresCompletos: "Victor Martinez*",
+      rifas: 1
+      },
+      {
+      id: 55,
+      nombresCompletos: "Nancy Garay Torres",
+      rifas: 1
+      },
+      {
+      id: 56,
+      nombresCompletos: "Jennifer Yahuarcani Ahuanari",
+      rifas: 1
+      },
+      {
+      id: 57,
+      nombresCompletos: "Veronica Pozo Palacios",
+      rifas: 1
+      },
+      {
+      id: 58,
+      nombresCompletos: "William Llacsa Romero",
+      rifas: 1
+      },
+      {
+      id: 59,
+      nombresCompletos: "Isaac Caballero Casanova",
+      rifas: 1
+      },
+      {
+      id: 60,
+      nombresCompletos: "Isaac Caballero Casanova",
+      rifas: 1
+      },
+      {
+      id: 61,
+      nombresCompletos: "Lidia Garay Garay",
+      rifas: 1
+      },
+      {
+      id: 62,
+      nombresCompletos: "Cesar Augusto Ramirez Estre",
+      rifas: 1
+      },
+      {
+      id: 63,
+      nombresCompletos: "Estefania Lizeth Jauregui Contreras",
+      rifas: 1
+      },
+      {
+      id: 64,
+      nombresCompletos: "Josue Alexander Fenco Bazan",
+      rifas: 1
+      },
+      {
+      id: 65,
+      nombresCompletos: "Stephania Briceño Marin",
+      rifas: 1
+      },
+      {
+      id: 66,
+      nombresCompletos: "Stephania Briceño Marin",
+      rifas: 1
+      },
+      {
+      id: 67,
+      nombresCompletos: "Jose Adalberto Guerrero Romero",
+      rifas: 1
+      },
+      {
+      id: 68,
+      nombresCompletos: "Marieta Luisa De La Cruz Jamanca",
+      rifas: 1
+      },
+      {
+      id: 69,
+      nombresCompletos: "Vannesa De Jesus Rodriguez Guerra",
+      rifas: 1
+      },
+      {
+      id: 70,
+      nombresCompletos: "Deny Manuel Ramirez Torre",
+      rifas: 1
+      },
+      {
+      id: 71,
+      nombresCompletos: "Deny Manuel Ramirez Torre",
+      rifas: 1
+      },
+      {
+      id: 72,
+      nombresCompletos: "Fiorella Amparo Lopez Zuñiga",
+      rifas: 1
+      },
+      {
+      id: 73,
+      nombresCompletos: "Frank Jesus Romero Olortegui",
+      rifas: 1
+      },
+      {
+      id: 74,
+      nombresCompletos: "Frank Jesus Romero Olortegui",
+      rifas: 1
+      },
+      {
+      id: 75,
+      nombresCompletos: "Marlith Romero Olortegui",
+      rifas: 1
+      },
+      {
+      id: 76,
+      nombresCompletos: "Marlith Romero Olortegui",
+      rifas: 1
+      },
+      {
+      id: 77,
+      nombresCompletos: "Lidia Olortegui Chiriboga",
+      rifas: 1
+      },
+      {
+      id: 78,
+      nombresCompletos: "Elida Chacon Barroz",
+      rifas: 1
+      }
+  ]
